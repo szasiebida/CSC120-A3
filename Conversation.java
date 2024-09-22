23eb4ebe0a1e259457f00ec2f273862b85703e45
sofia <173196545+szasiebida@users.noreply.github.com>
2024-09-22 14:17:48 -0400

    set up

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 class Conversation {
+  
+  int rounds;
+  Scanner input;
+
+  public Conversation(){
+    this.input = new Scanner(System.in);
+  }
+
+  public int howManyRounds(){
+    System.out.print("How many rounds would you like to play?");
+    this.rounds=this.input.nextInt();
+    return rounds;
+  }
 
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    Conversation myConversation = new Conversation();  
+    int rounds= myConversation.howManyRounds();
+
+    System.out.print("Hi there!  What's on your mind?");
+    for (int i=0; i<rounds; i++){
+      myConversation.input=new Scanner(System.in);
+      String sentence = myConversation.input.nextLine();
+      String[] words = sentence.split(" ");
+      if ("I" in words)
+    }
+
   }
+
 }
